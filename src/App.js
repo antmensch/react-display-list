5e2c7bb33fa84af9725c8eba24ba6f5082dfe542
antmensch <105740241+antmensch@users.noreply.github.com>
2022-06-15 21:44:00 +0300

    removed redundant array

--- src/App.js
+++ src/App.js
@@ -3,14 +3,10 @@ import './App.css';
 
 function App() {
   const list = ['James', 'John', 'Jim', 'Jack', 'Jason', 'Jacob', 'Jerry'];
-  let result = [];
-  list.map((elem, index) => {
-    result.push(<li key={index}>{elem}</li>);
-  });
   return (
     <div className="App">
       <ul>
-        {result}
+        {list.map( (elem, index) => <li key={index}>{elem}</li>)}
       </ul>
     </div>
   );
